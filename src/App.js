4090ba45800de85a74472f763b67635afa437abc
Aeri Choi <aeri.choi715@gmail.com>
2020-03-15 18:11:54 +0900

    feat(master): add Error Boundaries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 
-const Portals = () => {
-  return createPortal(<Message />, document.getElementById('portals'));
-};
+class ErrorMaker extends Component {
+  state = {
+    friends: ['aeri', 'jisu', 'daal', 'flynn']
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      })
+    }, 2000);
+  };
+
+  render() {
+    const { friends } = this.state;
+
+    return friends.map(friend => `${friend}`);
+  }
+}
+
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById('portals'));
+  }
+}
 
 const Message = () => 'Just Portals';
 
-const ReturnTypes = () => {
-  return (
-    'Hello React!'
-  );
-};
+class ReturnTypes extends Component {
+  render() {
+    return 'Hello React!';
+  }
+}
 
-const App = () => {
-  return (
-    <>
-      <ReturnTypes />
-      <Portals />
-    </>
-  );
-};
+const ErrorFallback = () => "에러가 발생 했습니다. 다음에 다시 이용해 주세요.";
+
+class App extends Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch(error, errorInfo) {
+    console.log(`에러: ${error} 발생. 에러 정보: ${JSON.stringify(errorInfo)}`);
+    this.setState({
+      hasError: true
+    })
+  }
+
+  render() {
+    const { hasError } = this.state;
+    return (
+      <>
+        { hasError ?
+          <ErrorFallback /> :
+          <>
+            <ReturnTypes />
+            <Portals />
+            <ErrorMaker />
+          </>
+        }
+      </>
+    );
+  }
+}
 
 export default App;
