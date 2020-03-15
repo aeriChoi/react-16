bbe763ed7df7de4f0ba6dc2f2d21968f74681c12
Aeri Choi <aeri.choi715@gmail.com>
2020-03-15 18:27:51 +0900

    feat(master): add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 
+const BoundaryHOC = ProtectedComponent =>
+  class Boundary extends Component {
+    state = {
+      hasError: false
+    };
+
+    componentDidCatch(error, errorInfo) {
+      this.setState({
+        hasError: true
+      })
+    };
+
+    render() {
+      const { hasError } = this.state;
+      if(hasError) {
+        return <ErrorFallback />
+      } else {
+        return <ProtectedComponent />
+      }
+    }
+  };
+
 class ErrorMaker extends Component {
   state = {
     friends: ['aeri', 'jisu', 'daal', 'flynn']
@@ -39,32 +61,16 @@ class ReturnTypes extends Component {
 const ErrorFallback = () => "에러가 발생 했습니다. 다음에 다시 이용해 주세요.";
 
 class App extends Component {
-  state = {
-    hasError: false
-  };
-
-  componentDidCatch(error, errorInfo) {
-    console.log(`에러: ${error} 발생. 에러 정보: ${JSON.stringify(errorInfo)}`);
-    this.setState({
-      hasError: true
-    })
-  }
 
   render() {
-    const { hasError } = this.state;
     return (
       <>
-        { hasError ?
-          <ErrorFallback /> :
-          <>
-            <ReturnTypes />
-            <Portals />
-            <ErrorMaker />
-          </>
-        }
+        <ReturnTypes />
+        <Portals />
+        <ErrorMaker />
       </>
     );
   }
 }
 
-export default App;
+export default BoundaryHOC(App);
